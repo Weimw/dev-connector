b6df51dcb64a1c529fe74d6af332c288cc65d2e9
Weimin Wan <weimin_wan@berkeley.edu>
2021-12-22 15:30:26 -0800

    add github repo api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { body, validationResult } = require('express-validator'); 
@@ -58,6 +60,30 @@ router.get('/user/:user_id', async (req, res) => {
         if (err.kind == 'ObjectId') {
             return res.status(400).json({ msg: 'There is no profile for this user' });
         }
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route GET api/profile/github/:username
+// @desc Get user repos from Github
+// @access Public
+router.get('/github/:username', async (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientID')}&client_sercet=${config.get('githubSecret')}`,
+            method:'GET',
+            headers: { 'user-agent': 'node.js' }
+        }
+
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+            if (response.statusCode != 200) {
+                return res.status(404).json({ msg: 'No github profile found'});
+            }
+            res.json(JSON.parse(body));
+        })
+    } catch (err) {
+        console.error(err.message);
         res.status(500).send('Server Error');
     }
 });
